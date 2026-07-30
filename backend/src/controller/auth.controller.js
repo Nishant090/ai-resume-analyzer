0b62022c061affe2c4c807989ac140218a7e2425
Luffy123@@@ <nishantkhatiwada6@gmail.com>
2026-07-30 17:14:09 +0545

    feat: implementing user login

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,5 +1,7 @@
 import * as authService from "../services/auth.service.js";
 
+
+//@POST /api/v1/auth/register
 export const register = async (req, res, next) => {
   try {
     const { user, token } = await authService.register(req.body);
@@ -17,6 +19,30 @@ export const register = async (req, res, next) => {
       user,
     });
   } catch (error) {
+    next(error);
+  }
+};
+
+//@POST /api/v1/auth/login
+export const login = async (req, res, next) => {
+  try {
+    const { user, token } = await authService.login(req.body);
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    return res.status(200).json({
+        success:true,
+        message:"Login successfull",
+        user
+
+    })
+  } catch (error) {
+   
     next(error);
   }
 };
--- backend/src/services/auth.service.js
+++ backend/src/services/auth.service.js
@@ -1,19 +1,21 @@
 import User from "../models/user.model.js";
 import generateToken from "../utils/generateToken.js";
 import ApiError from "../utils/apiError.js";
+ 
 
+//Register 
 export const register = async ({ name, email, password }) => {
   // all fields are required
-  
+
   if (!name || !email || !password) {
-    throw new ApiError(400,"All fields are required");
+    throw new ApiError(400, "All fields are required");
   }
 
   // checking existing user
   const existingUser = await User.findOne({ email });
 
   if (existingUser) {
-    throw new ApiError(409,"User already exists");
+    throw new ApiError(409, "User already exists");
   }
 
   //creating user
@@ -22,9 +24,9 @@ export const register = async ({ name, email, password }) => {
     email,
     password,
   });
-  
+
   const token = generateToken(user._id);
- 
+
   return {
     user: {
       id: user._id,
@@ -33,4 +35,38 @@ export const register = async ({ name, email, password }) => {
     },
     token,
   };
+};
+
+
+//Login
+export const login = async ({ email, password }) => {
+
+  // all fields are required
+  if (!email || !password) {
+    throw new ApiError(400, "email and password field are required");
+  }
+  const user = await User.findOne({ email }).select("+password");
+
+  // checking user
+  if (!user) {
+    throw new ApiError(401, "Invalid email or password");
+  }
+
+  //comparing password
+  const isPasswordValid = await user.comparePassword(password);
+
+  if (!isPasswordValid) {
+    throw new ApiError(401, "Invalid email or password");
+  }
+
+  const token = generateToken(user._id)
+
+  return {
+    user:{
+      id:user._id,
+      email:user.email,
+      name:user.name
+    },
+    token
+  }
 };
