281dc924b7ee4b06cf04fe41f7b89b753a7d4153
Luffy123@@@ <nishantkhatiwada6@gmail.com>
2026-08-01 21:07:34 +0545

    feat: implemented logiut

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -56,3 +56,16 @@ export const getMe= async(req,res)=>{
         }
     })
 }
+
+export const logout = (req, res) => {
+  res.clearCookie("token", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+  });
+
+  return res.status(200).json({
+    success: true,
+    message: "Logged out successfully",
+  });
+};
