4356c35464a7be20dfe4349b53b26fec515c48fa
Luffy123@@@ <nishantkhatiwada6@gmail.com>
2026-08-01 21:04:57 +0545

    feat:add routes for analysis

--- backend/src/routes/analysis.route.js
+++ backend/src/routes/analysis.route.js
@@ -1,10 +1,21 @@
 import { Router } from "express";
 import { protect } from "../middleware/auth.middleware.js";
 import upload from "../middleware/upload.middleware.js";
-import { uploadResume } from "../controller/analysis.controller.js";
+import {
+  deleteAnalysis,
+  getAnalysisById,
+  getHistory,
+  uploadResume,
+} from "../controller/analysis.controller.js";
 
 const router = Router();
+router.use(protect);
+router.post("/uploads", upload.single("resume"), uploadResume);
 
-router.post("/uploads", protect, upload.single("resume"), uploadResume);
+router.get("/history", getHistory);
+
+router.get("/:id", getAnalysisById);
+
+router.delete("/:id", deleteAnalysis);
 
 export default router;
